ecb7883f74c9cf2c8ec21021ecfeb2dd4d324d86
chris <65360545+chrisortega1928@users.noreply.github.com>
2022-06-02 19:40:14 +0000

    50% done with module

--- myApp.js
+++ myApp.js
@@ -1,6 +1,29 @@
 let express = require('express');
 let app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  //res.send("Hello Express");
+  res.sendFile(absolutePath = __dirname + "/views/index.html");
+});
+
+app.get("/json", (req, res) => {
+  if (process.env['MESSAGE_STYLE'] === "uppercase") {
+  res.json({
+    message: "Hello json".toUpperCase()
+  });
+} else {
+  res.json({
+    message: "Hello json"
+  });
+}
+});
+
+console.log("Hello World");
+
+
+
 
 
 
